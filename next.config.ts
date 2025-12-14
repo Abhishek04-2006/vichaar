8e12866c05a710c33ebf46dc926c3e814b7eb2e6
Abhishek04-2006 <tiwarivineeta193@gmail.com>
2025-12-14 13:00:29 +0530

    Add Cloudinary domain to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
